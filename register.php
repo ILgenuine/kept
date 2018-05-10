b93776956fabde4b9ea9aa5f4d29b2fcaed97296
chut1382538 <jaurpat.soontornpak@gmail.com>
2018-05-10 13:28:22 +0700

    Gear

---
+++ register.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <?php include_once('inc/header.php'); ?>
+  </head>
+
+  <body>
+
+    <?php include_once('inc/navbar.php'); ?>
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="text-blue col-lg-12 con-1 p-4">
+			<h1>REGISTER</h1>
+		</div>
+
+		<div class="col-lg-12">
+			<form>
+				  <div class="form-group row p-2" >
+				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen">Email</label>
+				    <div class="col-sm-4">
+				      <input type="text"  class="form-control">
+				    </div>
+				  </div>
+
+				  <div class="form-group row p-2">
+				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen">Password</label>
+				    <div class="col-sm-4">
+				      <input type="password" class="form-control">
+				    </div>
+				  </div>
+
+				  <div class="col-sm-4">
+				  	
+				  </div>
+					
+
+			</form>	
+		</div>
+
+	<div class="col-lg-12 p-4">
+				<div class="text-center ">
+					<button type="button" class="btn btn-blue login-buttom but-11" >Sing Up with Email</button>
+				</div>
+				<hr class="blue-hr" style="width: 160px;">
+</div>
+
+			<div class="col-lg-12 p-1">
+				<div class="text-center">
+				<button type="button" class="btn btn-blue login-buttom but-11" ><i class="fab fa-facebook-square"></i> Sing Up with Facebook</button>
+					
+				</div>
+			</div>
+
+			<div class="col-lg-12 p-3">
+				<div class="text-center">
+					<button type="button" class="btn btn-blue btn-red login-buttom but-11"  ><i class="fab fa-google"></i>Sing Up with google</button>
+				</div>
+			</div>		
+
+
+
+</div>
+</div>
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     <?php include_once('inc/footer.php'); ?>
+
+  </body>
+</html>
+<?php include_once('inc/footer-script.php'); ?>
