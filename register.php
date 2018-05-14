c81c170f3e26b6a4f32d6933af442a53a7ce3ee1
chut1382538 <jaurpat.soontornpak@gmail.com>
2018-05-14 08:15:09 +0700

    add login.php,register.php,contactus.php #gia 8.14 14/05/2561

--- register.php
+++ register.php
@@ -8,27 +8,82 @@
 
     <?php include_once('inc/navbar.php'); ?>
 
+    <div class="wrapper">
+
+
+      <div class="header-box">
+          <div class="row">
+            <div class="col-lg-8 col-md-12 col-12 pb-5">
+                <div id="demo" class="carousel slide header-slide" data-ride="carousel">
+                  <!-- The slideshow -->
+                  <div class="carousel-inner mb-3">
+                    <div class="carousel-item active">
+                      <img width='100%' height='100%' src="assets/images/all.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/slide.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/logo/color.png" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/slide.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/slide.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/slide.jpg" alt="">
+                    </div>
+                    <div class="carousel-item">
+                      <img width='100%' height='100%' src="assets/images/slide.jpg" alt="">
+                    </div>
+                  </div>
+                  <!-- Indicators -->
+                  <ul class="carousel-indicators mt-5">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                    <li data-target="#demo" data-slide-to="3"></li>
+                    <li data-target="#demo" data-slide-to="4"></li>
+                    <li data-target="#demo" data-slide-to="5"></li>
+                    <li data-target="#demo" data-slide-to="6"></li>
+                  </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 col-12 text-lg-left text-md-center text-center">
+                <div class=""><font class="header-title text-blue">KEPT</font></div>
+                <div class="mb-4"><font class="header-excerpt text-blue">Keep Everything Perfectly Tidy</font></div>
+                <div class="get-start mb-md-4 mb-4">
+                  <button type="button" class="btn btn-lightblue ">GET  START</button>
+                </div>
+            </div>
+          </div>
+      </div>
+
+      <div>
+
 
 
 <div class="container">
 	<div class="row">
 		<div class="text-blue col-lg-12 con-1 p-4">
-			<h1>REGISTER</h1>
+			<h2>REGISTER</h2>
 		</div>
 
 		<div class="col-lg-12">
 			<form>
 				  <div class="form-group row p-2" >
-				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen">Email</label>
+				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen re-but-1 re-can">Email</label>
 				    <div class="col-sm-4">
-				      <input type="text"  class="form-control">
+				      <input type="text"  class="form-control re-width re-width414">
 				    </div>
 				  </div>
 
 				  <div class="form-group row p-2">
-				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen">Password</label>
+				    <label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen re-but-1 re-can">Password</label>
 				    <div class="col-sm-4">
-				      <input type="password" class="form-control">
+				      <input type="password" class="form-control re-width re-width414">
 				    </div>
 				  </div>
 
@@ -42,21 +97,24 @@
 
 	<div class="col-lg-12 ">
 				<div class="text-center p-3">
-					<button type="button" class="btn btn-blue login-buttom but-11" >Sing Up with Email</button>
+					<button type="button" class="btn btn-blue login-buttom but-11 re-buttom-tap re-width768 " >Sing Up with Email</button>
 				</div>
 				<hr class="blue-hr" style="width: 160px;">
 </div>
 
 			<div class="col-lg-12 p-1">
 				<div class="text-center">
-				<button type="button" class="btn btn-blue login-buttom but-11 text-left px-5"><i class="fab fa-facebook-square"></i><font class="pl-4">Sing Up with Facebook</font></button>
-					
+					<button type="button" class="btn btn-blue login-buttom but-11 re-buttom-tap re-width768 ">
+						<i class="fab fa-facebook-square  re-tap1366 re-1-tap1366 re-tap1024 re-tap320"></i>
+							<font class="re-tap ">Sing Up with Facebook</font></button>
 				</div>
 			</div>
 
 			<div class="col-lg-12 p-3">
 				<div class="text-center">
-					<button type="button" class="btn btn-blue btn-red login-buttom but-11 text-left px-5"><i class="fab fa-google"></i><font class="pl-4">Sing Up with google</font></button>
+					<button type="button" class="btn btn-blue btn-red login-buttom but-11 re-buttom-tap re-width768">
+						<i class="fab fa-google  re-tap1366 re-2-tap1366 re-1-tap1024 re-tap320-2"></i>
+							<font class="re-tap ">Sing Up with google</font></button>
 				</div>
 			</div>		
 
