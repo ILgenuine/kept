d9140da60e1c9ae48357f74008479c85c6546e63
gggia <gggia36@gmail.com>
2018-05-15 15:53:03 +0700

    pull css/custom.css 15.52 15/05/2561 #gia

--- profile.php
+++ profile.php
@@ -12,28 +12,32 @@
 
 
 
+      <div style="position: relative;">
+        <div class="nav flex-column nav-pills mybox-tab-position" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <a class="nav-link mybox-tab-style text-center">ข้อมูลการติดต่อ</a>
+          <a class="nav-link mybox-tab-style text-center navs-active">เสียงตอบรับ / ข้อเสนอแนะ</a>
+        </div>
+      </div>
 
 
 
 
-<div class="row">
-				<div class="col-lg-2">
-			       <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-				           <a class="nav-link mybox-tab-style text-center">ข้อมูลการติดต่อ</a>
-					       <a class="nav-link mybox-tab-style text-center navs-active">เสียงตอบรับ / ข้อเสนอแนะ</a>
-			        </div>
-				</div>
+<div class="row profile-position">
+
+
+
+	<div class="col-lg-2 col-sm-1 col-12"></div>
 
 
 				
 
 
 
-	<div class="col-lg-6 pt-4">
+	<div class="col-lg-6 col-sm-7 col-12 pt-4">
 			<div class="col-12">
 				<div class="form-group row p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">คำนำหน้า</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">คำนำหน้า</label>
+					<div class="col-lg-8 ">
 							<select  class="custom-select" id="inputGroupSelect01" style="width: 200px; ">
 					           <option selected></option>
 					           <option value="1">One</option>
@@ -50,8 +54,8 @@
 
 			<div class="col-12">
 				<div class="form-group row p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ชื่อ</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ชื่อ</label>
+					<div class="col-lg-8 ">
 						<input type="text"  class="form-control">
 					</div>
 	
@@ -62,8 +66,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">นามสกุล</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">นามสกุล</label>
+					<div class="col-lg-8 ">
 						<input type="text" class="form-control">
 					</div>
 	
@@ -75,8 +79,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">เบอร์โทร</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">เบอร์โทร</label>
+					<div class="col-lg-8 ">
 						<input type="text" class="form-control">
 					</div>
 	
@@ -87,8 +91,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">อีเมล์</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">อีเมล์</label>
+					<div class="col-lg-8 ">
 						<input type="text"  class="form-control">
 					</div>
 	
@@ -100,8 +104,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ที่อยุ่ 1</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ที่อยุ่ 1</label>
+					<div class="col-lg-8 ">
 						<input type="text"  class="form-control">
 					</div>
 	
@@ -113,8 +117,8 @@
 			<div class="col-12">
 				<form>
 				  <div class="form-group row">
-				    <label for="colFormLabelSm" class="col-sm-4 col-form-label col-form-label-sm"></label>
-				    <div class="col-sm-8 text-right">
+				    <label for="colFormLabelSm" class="col-lg-4 col-form-label col-form-label-sm"></label>
+				    <div class="col-lg-8 text-right">
 				      <button class="button plus-btn mr-2"><i class="fa fa-plus"></i></button>
 				    </div>
 				    	
@@ -126,8 +130,8 @@
 			<div class="col-12">
 				<form>
 				  <div class="form-group row">
-				    <label for="colFormLabelSm" class="col-sm-4 col-form-label col-form-label-sm"></label>
-				    <div class="col-sm-8 text-right">
+				    <label for="colFormLabelSm" class="col-lg-4 col-form-label col-form-label-sm"></label>
+				    <div class="col-lg-8 text-right">
 						<div style="width: 257px;  margin-right: 316px;margin: auto;">
 							<hr class="blue-hr">
 						</div>				   
@@ -139,8 +143,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ชื่อผู้ใช้งาน</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">ชื่อผู้ใช้งาน</label>
+					<div class="col-lg-8 ">
 						<input type="text"  class="form-control " style="margin-top: 35px;">
 					</div>
 				</div>
@@ -150,8 +154,8 @@
 
 			<div class="col-12">
 				<div class="form-group row  p-2" >
-					<label class="col-sm-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">รหัสผ่าน</label>
-					<div class="col-sm-8 ">
+					<label class="col-lg-4 col-form-label text-11 text-lightblue login-text-cen text-mail11">รหัสผ่าน</label>
+					<div class="col-lg-8 ">
 						<input type="text"  class="form-control">
 					</div>
 				</div>
@@ -181,8 +185,8 @@
 
 		
 
-				<div class="col-lg-4 pt-4">	
-					<div class="profile-img-frame">
+				<div class="col-lg-4 col-sm-4 col-12 pt-4">	
+					<div class="profile-img-frame" style="margin: auto;">
 						<div class="profile-img-frame">
 
 						<div class="mybox-image-frame">
